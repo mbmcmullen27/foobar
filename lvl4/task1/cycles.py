a0395302e3b6e9ab6347bfb172ca46ef510fa865
fishbot <mbmcmullen27@gmail.com>
2021-03-21 15:27:13 -0500

    refactor again, phaseone as a method

--- lvl4/task1/cycles.py
+++ lvl4/task1/cycles.py
@@ -37,57 +37,72 @@ def solution(banana_list):
     if len(banana_list) == 1: return 1
     prefs=dict()
 
+    def preferences(x):
+        return len(prefs[x])
+    
     for i in range(len(banana_list)):
         root=banana_list[i]
         prefs[i]=matchlist(i,banana_list)
 
 
     for i in range(len(prefs)):
+        prefs[i]=sorted(prefs[i],key=preferences)
         print '[{}]:{}'.format(i,prefs[i])
 
     proposals=dict()
     leftovers=0
     
     stack=list(range(len(banana_list)))
-    def preferences(x):
-        return len(prefs[x])
-    stack=sorted(stack,key=preferences)
+    
+    stack=sorted(stack,key=preferences,reverse=True)
 
     while stack:
+        print stack
         index=stack.pop(0)
         pref_list=prefs[index]
         assigned=proposals.values()
         sutors=proposals.keys()
         if not pref_list: leftovers+=1
+        for i in range(len(prefs)):
+            print '\t[{}]:{}'.format(i,prefs[i])
         
-        while pref_list:
+        if pref_list :
             p=prefs[index][0]
-            print '{}->{} : {}'.format(index,p,[banana_list[i] for i in pref_list])
+            # print '{}->{} : {}'.format(index,p,[banana_list[i] for i in pref_list])
+            print '{}->{} '.format(index,p)
 
             if not p in assigned: 
+                print 'ACCEPT'
                 proposals[index]=p
-                break
             else:
-                # reject=sutors[assigned.index(p)]
-                # proposals[index]=p
-                prefs[index].pop(0)
-                # stack.insert(0,index)
-                # break               
+                # decide who to reject based on p's preference
+                # if index is higher on p's list reject the trainer p is assigned too
+                reject=sutors[assigned.index(p)]
+                print '{} HOLDS {}'.format(reject,p)
+                # if prefs[p].index(reject)>prefs[p].index(index):
+                proposals[index]=p
+                proposals.pop(reject)
+                print '{} REJECTS {}'.format(p,reject)
+                prefs[p].pop(prefs[p].index(reject))
+                prefs[reject].pop(0)
+                stack.insert(0,reject)
+                # else:
+                #     print '{} REJECTS {}'.format(p,index)
+                #     prefs[index].pop(0)
+                #     stack.insert(0,index)               
+        print proposals
+            
     print 'proposals: {}'.format(proposals)
-    # for i in range(len(prefs)):
-    #     print '[{}]:{}->{}:{}'.format(i,banana_list[i],banana_list[proposals[i]],[banana_list[k] for k in prefs.get(i)])    
     for i in range(len(prefs)):
         print '[{}]:{}'.format(i,prefs[i])
-    # WE DONT PREFER ANYONE LESS -only remove from a list if your explicitly rejected
+
     for p in range(len(proposals)):
-        #p and proposals[p] are both indexes in banana_list and pref
-        #remove anyone p prefers less than the current proposal (noone see line-=2 )
         held=proposals[p]
         # print '\n{}'.format(banana_list)
         # print 'i:{} holds a proposal from i:{}'.format(p,held) 
-        if len(prefs[p]) >= 2:
+        # if len(prefs[p]) >= 2:
             #check for cycles
-            cycles(p,proposals,prefs)
+            # cycles(p,proposals,prefs)
 
     print 'proposals: {}'.format(proposals)
 
@@ -98,29 +113,8 @@ def cycles(p,proposals,prefs):
     for i in range(len(prefs)):
         print '[{}]:{}'.format(i,prefs[i])
     history=dict()
-    def iterate(p):
-        if len(prefs[p])<=1:
-            return
-        
-        q=prefs[p][1]
-
-        if len(prefs[q])<=1:
-            return
-
-        suspect=prefs[q][-1]
-        
-        if history.get(suspect):
-            prefs[suspect].pop(prefs[suspect].index(q))
-            prefs[q].pop(prefs[q].index(suspect))
-            #found a loop
-            #pi+1 rejects qi
-            print 'loop! {} rejects {}'.format(suspect,q)
-        else:
-            print 'p:{} -> q:{} -> q\':{}'.format(p,q,suspect)
-            history[p]=True
-            iterate(suspect)
-        
-    iterate(p)
+    # def iterate(p):
+    #    iterate(p)
 
 
 # print solution([9,10,10,20,13,79,86,101,13,900,48,1,3])
